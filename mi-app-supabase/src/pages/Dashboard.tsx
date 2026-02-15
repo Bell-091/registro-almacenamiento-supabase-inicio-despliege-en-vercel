28266e27117f646e90205481ce7bd1943b1c4474
Reinaldo <opaso122@gmail.com>
2026-02-15 19:27:20 -0400

    Estado estable antes de refactorizar arqutectura

--- mi-app-supabase/src/pages/Dashboard.tsx
+++ mi-app-supabase/src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ const DashboardPage: React.FC = () => {
   return (
     <div>
       <h1>Bienvenido a la pagina de descripcion de las aplicaciones...</h1>
-      <p>Alicaciones disponibles en apps del usuario calculadora de diferencia de tiempos y lista de tareas en la nube.</p>
+      <p>Aplicaciones disponibles en apps del usuario calculadora de diferencia de tiempos y lista de tareas en la nube.</p>
     </div>
   );
 };
